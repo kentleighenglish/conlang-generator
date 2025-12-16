148392f0556aa8bf9db226e5d93e28e9b0d446ec
Kentleigh English <kentleigh@ikengainnovations.com>
2025-12-16 01:16:29 +0000

    disable dragging carousel

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,35 +1,26 @@
 <script setup lang="ts">
 const tabCarousel = useTemplateRef("dashboardTabCarousel");
 const activeTab = ref<number>(0); // this is changed by UTabs
-// const activeTab = ref<string>(selectedTab.value);
+
+const DashboardTranslator = resolveComponent("DashboardTranslator");
 const dashboardNav = [
   {
     icon: "i-ion:language",
     label: "Languages",
     value: 0,
+    node: h("div", { class: "w-full h-full" }, ["Hello World"]),
   },
   {
     icon: "i-oui:generate",
     label: "Translator",
-    value: 1
+    value: 1,
+    node: h(DashboardTranslator),
   }
 ];
 
-const DashboardTranslator = resolveComponent("DashboardTranslator");
-const items = [
-    {
-        node: h(DashboardTranslator)
-    },
-    {
-        node: h("div", { class: "w-full h-full" }, ["Hello World"])
-    }
-];
-
-// const tabClass = ref<{ "tabTransition--left"?: boolean; "tabTransition--right"?: boolean }>({ "tabTransition--right": true });
 
 watch(activeTab, (newTab) => {
     const newIndex = dashboardNav.findIndex((tab) => tab.value === newTab);
-
     tabCarousel.value?.emblaApi?.scrollTo(newIndex);
 });
 </script>
@@ -38,12 +29,12 @@ watch(activeTab, (newTab) => {
         <template #header>
             <UDashboardNavbar title="Conlang Generator">
                 <template #right>
-                    <UTabs v-model="activeTab" :items="dashboardNav" size="lg" variant="link" :content="false" />
+                    <UTabs v-model="activeTab" :items="dashboardNav" size="lg" variant="pill" :content="false" />
                 </template>
             </UDashboardNavbar>
         </template>
         <template #body>
-            <UCarousel v-slot="{ item }" ref="dashboardTabCarousel" :items="items" class="w-full h-full">
+            <UCarousel v-slot="{ item }" ref="dashboardTabCarousel" :items="dashboardNav" :watch-drag="false" class="w-full h-full">
                 <component :is="item.node" />
                 <!-- <div class="w-full h-full">
                     <h1>HELLO WORLD</h1>
