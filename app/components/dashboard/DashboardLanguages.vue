b0bdeab01038b96d777e38a778581a1764b0b234
Kentleigh English <kentleigh@ikengainnovations.com>
2025-12-18 00:23:04 +0000

    fix: I forgot how pinia reactivity works

--- app/components/dashboard/DashboardLanguages.vue
+++ app/components/dashboard/DashboardLanguages.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import type { NavigationMenuItem, TableColumn } from '@nuxt/ui';
-import { useLanguageStore, type SoundShift } from '~/store/languages';
+import type { NavigationMenuItem, TableColumn } from "@nuxt/ui";
+import { useLanguageStore, type SoundShift } from "~/store/languages";
 
-const { languages, addLanguage, setCurrentLanguage, currentLanguage, currentLanguageId } = useLanguageStore();
+const languageStore = useLanguageStore();
 
 const addLanguageModalOpen = ref<boolean>(false);
-const navItems = computed<NavigationMenuItem[]>(() => ([
-  ...languages.map((language) => ({
+const navItems = computed<NavigationMenuItem[]>(() => [
+  ...languageStore.languages.map((language) => ({
     label: language.name,
-    active: language.id === currentLanguageId,
-    onSelect: () => setCurrentLanguage(language.id),
+    active: language.id === languageStore.currentLanguageId,
+    onSelect: () => languageStore.setCurrentLanguage(language.id),
   })),
   {
     label: "Add Language",
@@ -17,27 +17,34 @@ const navItems = computed<NavigationMenuItem[]>(() => ([
     onSelect: (e: Event) => {
       e.preventDefault();
       addLanguageModalOpen.value = true;
-    }
-  }
-]));
+    },
+  },
+]);
 
 const tableColumns: TableColumn<SoundShift>[] = [
-    {
-        id: "title",
-        header: "Language"
-    },
+  {
+    id: "index",
+    cell: () => "1",
+  },
+  {
+    accessorKey: "from",
+    header: "Language",
+  },
 ];
 
 const addLanguageName = ref<string>("");
 const onAddLanguage = () => {
   if (addLanguageName.value) {
-    addLanguage({ name: addLanguageName.value });
+    languageStore.addLanguage({ name: addLanguageName.value });
   }
 };
-
 </script>
 <template>
-  <UDashboardGroup storage="local" storage-key="languagesDashboard" class="h-full">
+  <UDashboardGroup
+    storage="local"
+    storage-key="languagesDashboard"
+    class="h-full"
+  >
     <UModal ref="addLanguageModal" v-model:open="addLanguageModalOpen">
       <template #body>
         <UForm @submit="onAddLanguage">
@@ -47,21 +54,17 @@ const onAddLanguage = () => {
       </template>
     </UModal>
     <UDashboardSidebar collapsible :ui="{ root: 'min-h-full' }">
-        <template #header>
-            <h1 class="prose">Languages</h1>
-        </template>
-        <template #default>
-            <UNavigationMenu
-                highlight
-                :items="navItems"
-                orientation="vertical"
-            />
-        </template>
+      <template #header>
+        <h1 class="prose">Languages</h1>
+      </template>
+      <template #default>
+        <UNavigationMenu highlight :items="navItems" orientation="vertical" />
+      </template>
     </UDashboardSidebar>
     <UDashboardPanel class="h-full min-h-full overflow-hidden">
       <template #body>
-        <UCard v-if="currentLanguage" variant="outline">
-          <UTable :columns="tableColumns" :data="currentLanguage.soundShifts" />
+        <UCard v-if="languageStore.currentLanguage" variant="outline">
+          <UTable :columns="tableColumns" :data="languageStore.currentLanguage.soundShifts" />
         </UCard>
         <UEmpty v-else title="No language selected" />
       </template>
