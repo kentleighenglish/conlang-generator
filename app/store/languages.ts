8b04575e56303266bb98eb729e84f4278780626f
Kentleigh English <kentleigh@ikengainnovations.com>
2025-12-18 00:06:54 +0000

    feat: update language store to handle adding and setting language, and storing in LocalStorage

--- app/store/languages.ts
+++ app/store/languages.ts
@@ -1,22 +1,70 @@
+import { v4 as uuidv4 } from "uuid";
 import { defineStore } from "pinia";
 
-type Language = {
-    name: string;
-    vowelShiftsEnabled: boolean;
-    consonantShiftsEnabled: boolean;
-    vowelShifts: [];
-    consonantShifts: [];
+export type SoundShift = {
+  from: string;
+  to: string;
+  preceding?: string;
+  succeeding?: string;
+  startOnly?: boolean;
+  endOnly?: boolean;
 }
 
+type Language = {
+  id: string;
+  name: string;
+  soundShifts: SoundShift[];
+};
+
 export const useLanguageStore = defineStore("language", () => {
-    const languages = ref<Language[]>([]);
+  const languages = ref<Language[]>([]);
+  const currentLanguageId = ref<string | null>(null);
 
-    const storedLanguages = localStorage.getItem("languages");
-    if (storedLanguages) {
+  const currentLanguage = computed<Language | undefined>(() => languages.value.find(({ id }) => currentLanguageId.value = id));
+
+  const init = () => {
+    if(window && "localStorage" in window) {
+      const storedLanguages = localStorage.getItem("languages");
+      const storedCurrentLanguage = localStorage.getItem("currentLanguage");
+      if (storedLanguages) {
         languages.value = JSON.parse(storedLanguages) as Language[];
+      }
+      if (storedCurrentLanguage) {
+        currentLanguageId.value = storedCurrentLanguage;
+      }
     }
+  };
 
-    return {
-        languages
-    };
+  const saveStoredLanguages = () => {
+    if (window && "localStorage" in window) {
+      localStorage.setItem("languages", JSON.stringify(languages.value));
+    }
+  };
+
+  const setCurrentLanguage = (id: string) => {
+    currentLanguageId.value = id;
+
+    if (window && "localStorage" in window) {
+      localStorage.setItem("currentLanguage", currentLanguageId.value);
+    }
+  };
+
+  const addLanguage = ({ name }: { name: string }) => {
+    languages.value.push({
+      id: uuidv4(),
+      name,
+      soundShifts: [],
+    });
+
+    saveStoredLanguages();
+  };
+
+  return {
+    languages,
+    addLanguage,
+    currentLanguage,
+    currentLanguageId,
+    setCurrentLanguage,
+    init,
+  };
 });
