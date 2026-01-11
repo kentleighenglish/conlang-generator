39ceabb2478cb5b207f72f67eab79784a50fd34a
Kentleigh English <kentleigh@ikengainnovations.com>
2026-01-11 01:51:37 +0000

    feat: add title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,11 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/eslint", "@pinia/nuxt"],
   css: ["~/assets/css/main.css"],
+  app: {
+    head: {
+      title: "Conlang Generator",
+    },
+  },
   nitro: {
     devStorage: {
       translation: {
