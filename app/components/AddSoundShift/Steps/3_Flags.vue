bcd1f986b4b8f1aee915b6ad58f5bc9bd61351fa
Kentleigh English <kentleigh@ikengainnovations.com>
2026-01-18 17:27:33 +0000

    feat: add flags and chaos sections

--- app/components/AddSoundShift/Steps/3_Flags.vue
+++ app/components/AddSoundShift/Steps/3_Flags.vue
@@ -10,9 +10,13 @@ const form = useTemplateRef("form");
 </script>
 <template>
   <UForm v-if="model" ref="form" class="w-full space-y-4" :schema="schema">
-    <UFormField label="From">
-      <TableSelectSound v-model="model.from" lang="en" />
-      <!-- Step 3: Prevent multiple iterations,  -->
-    </UFormField>
+      <UFormField orientation="horizontal" class="w-full">
+        <UCheckbox
+          v-model="model.preventMultipleIterations"
+          class="flex align-center"
+          label="Prevent Multiple Iterations"
+          description="Will ensure this sound shift is not affected by further sound shifts"
+        />
+      </UFormField>
   </UForm>
 </template>
--- app/components/CustomSlider.vue
+++ app/components/CustomSlider.vue
@@ -1,3 +1,8 @@
+<script lang="ts" setup>
+import type { SelectProps } from "@nuxt/ui";
+
+defineProps<SelectProps>();
+</script>
 <template>
   <div class="flex gap-3">
     <div class="text-primary">{{ $attrs.modelValue }}</div>
