ccaa91913727636186ae8936088a2c7f7dc7723f
Kentleigh English <kentleigh@ikengainnovations.com>
2026-01-20 17:49:52 +0000

    feat: implement conditions and flags into apply sound shift

--- app/utils/applySoundShifts.ts
+++ app/utils/applySoundShifts.ts
@@ -1,9 +1,38 @@
-export default function (ipa: string, soundShifts: SoundShift[]): string {
-  return ipa.split("").map((char, _index) => {
+import { ShiftModeEnum, type SoundShift } from "~~/types/soundShift";
+
+const applySoundShift = function (ipa: string, soundShifts: SoundShift[]): string {
+  return ipa.split("").map((char, index) => {
     const matched = soundShifts.find((shift) => shift.from === char);
     if (!matched)
       return char;
     
-    return matched.to;
+    const { startOnly, endOnly, leading, trailing, shiftMode, allowMultipleIterations } = matched;
+
+    // check conditions
+    if (
+      (startOnly && index !== 0) ||
+      (endOnly && (index + 1) !== ipa.length) ||
+      (trailing && ipa[index + 1] !== trailing) ||
+      (leading && ipa[index - 1] !== leading)
+    ) {
+      return char;
+    }
+
+    let out = char;
+    if (shiftMode === ShiftModeEnum.dropSound) {
+      out = "";
+    } else if (shiftMode === ShiftModeEnum.changeVoice) {
+      const voicingAlternative = getVoicingAlternative(matched.from);
+      out = voicingAlternative?.ligature ?? char;
+    } else {
+      out = matched.to!;
+    }
+
+    if (allowMultipleIterations) {
+      out = applySoundShift(out, soundShifts);
+    }
+    return out;
   }).join("");
 }
+
+export default applySoundShift;
